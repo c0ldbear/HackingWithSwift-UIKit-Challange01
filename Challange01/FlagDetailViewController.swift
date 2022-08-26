0dafc46e00507e1c9b54ef7727a274a27827c274
teddy <teddyjh@protonmail.com>
2022-08-26 08:19:25 +0200

    Add flag and name to flag detail view & constraints to vc

--- Challange01/FlagDetailViewController.swift
+++ Challange01/FlagDetailViewController.swift
@@ -8,8 +8,25 @@
 import UIKit
 
 class FlagDetailViewController: UIViewController {
+    @IBOutlet var flagImageView: UIImageView!
+    
+    var selectedFlag: String?
+    private var flagName: String {
+        if let selectedFlag = selectedFlag,
+            let index = selectedFlag.range(of: "@")?.lowerBound {
+            let flag = String(selectedFlag[..<index])
+            return flag.count < 3 ? flag.uppercased() : flag.capitalized
+        }
+        return ""
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = flagName
+        flagImageView.layer.borderWidth = 1
+        flagImageView.layer.borderColor = UIColor.lightGray.cgColor
+        flagImageView.image = UIImage(named: selectedFlag!)
+        
     }
 }
